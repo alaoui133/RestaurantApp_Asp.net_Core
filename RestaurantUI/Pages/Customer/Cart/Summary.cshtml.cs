fc91ac49a6792a2b799dfb78e7eebf28c8f933a8
alaoui133 <alaoui133@gmail.com>
2023-06-07 23:14:08 +0100

    summary updates

--- RestaurantUI/Pages/Customer/Cart/Summary.cshtml.cs
+++ RestaurantUI/Pages/Customer/Cart/Summary.cshtml.cs
@@ -1,12 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Restaurant.DAL.Interfaces;
+using Restaurant.Models;
+using System.Security.Claims;
 
 namespace RestaurantUI.Pages.Customer.Cart
 {
     public class SummaryModel : PageModel
     {
-        public void OnGet()
+        public readonly IUnitOfWork _UnitOfWork;
+        public Order Order { get; set; }
+        public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+
+        public SummaryModel( IUnitOfWork unitOfWork)
         {
+            _UnitOfWork = unitOfWork;
+            Order = new Order();
+        }
+        public async void OnGet()
+        {
+            var UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (UserId != null)
+            {
+                ShoppingCartList = await _UnitOfWork.ShoppingCartRepo.GetAll(
+                    
+                    filter: u=>u.UserId == UserId ,
+                    includeProperties: "ApplicationUser,MenuItem"
+                    );
+
+                foreach (var cart in ShoppingCartList)
+                {
+                    Order.OrderTotal += (cart.MenuItem.Price + cart.Count);
+                }
+
+                ApplicationUser AppUser = ShoppingCartList.First().ApplicationUser;
+                Order.PickUpName = AppUser.FirstName +" "+ AppUser.LastName;
+                Order.PhoneNumber = AppUser.PhoneNumber;
+                Order.OrderDate = DateTime.Now;
+                Order.PickUpTime = DateTime.Now;
+            }
         }
     }
 }
