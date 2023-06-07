df714002fcf297c00c7b8b1011a848ff0b1f9586
alaoui133 <alaoui133@gmail.com>
2023-06-07 23:22:46 +0100

    add binding

--- RestaurantUI/Pages/Customer/Cart/Summary.cshtml.cs
+++ RestaurantUI/Pages/Customer/Cart/Summary.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Restaurant.DAL.Interfaces;
@@ -6,10 +7,12 @@ using System.Security.Claims;
 
 namespace RestaurantUI.Pages.Customer.Cart
 {
+    [Authorize]
     public class SummaryModel : PageModel
     {
-        public readonly IUnitOfWork _UnitOfWork;
+        [BindProperty]
         public Order Order { get; set; }
+        public readonly IUnitOfWork _UnitOfWork;
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
 
         public SummaryModel( IUnitOfWork unitOfWork)
@@ -25,7 +28,7 @@ namespace RestaurantUI.Pages.Customer.Cart
                 ShoppingCartList = await _UnitOfWork.ShoppingCartRepo.GetAll(
                     
                     filter: u=>u.UserId == UserId ,
-                    includeProperties: "ApplicationUser,MenuItem"
+                    includeProperties: "ApplicationUser,MenuItem,MenuItem.Category,MenuItem.FoodType"
                     );
 
                 foreach (var cart in ShoppingCartList)
