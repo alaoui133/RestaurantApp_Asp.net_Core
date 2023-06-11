03ceb0eabd3862b8e8e22a8572d150b83ba250bd
alaoui133 <alaoui133@gmail.com>
2023-06-11 19:25:15 +0100

    stripe setting done

--- RestaurantUI/Pages/Customer/Cart/OrderConfirm.cshtml.cs
+++ RestaurantUI/Pages/Customer/Cart/OrderConfirm.cshtml.cs
@@ -1,12 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Restaurant.DAL.Interfaces;
+using Restaurant.Models;
+using Restaurant.Utilitiy;
+using Stripe.Checkout;
 
 namespace RestaurantUI.Pages.Customer.Cart
 {
     public class OrderConfirmModel : PageModel
     {
-        public void OnGet()
+        private readonly IUnitOfWork _unitOfWork;
+        public int OrderId { get; set; }
+
+        public OrderConfirmModel(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task OnGet(int id)
+        {
+            Order order = await _unitOfWork.OrderRepo.GetById(o=> o.Id == id);
+            if (order.sessionId != null) 
+            {
+                var service = new SessionService();
+                Session session = service.Get(order.sessionId);
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+                    order.Status = ConstRoleDef.StatusCompleted;
+                    order.PaymentIntentId = session.PaymentIntentId;
+
+                    // remove Shopping Cart
+                    List<ShoppingCart>list = (await _unitOfWork.ShoppingCartRepo.GetAll(
+                        filter:u=>u.UserId == order.UserId)).ToList();
+                    _unitOfWork.ShoppingCartRepo.removeRange(list);
+                    await _unitOfWork.Save();
+                    OrderId = id;
+
+                }
+            }
         }
     }
 }
