52366937ab8dd8f14e6087ebc84e8e69ed4e27cc
alaoui133 <alaoui133@gmail.com>
2023-06-16 23:14:27 +0100

    working on orderDetails frontend templtae

---
+++ Restaurant.Models/ViewModels/OrderDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models.ViewModels
+{
+
+    public class OrderDetailsViewModel
+    {
+        public Order order { get; set; }
+        public IEnumerable<OrderDetails> orderDetails { get; set; }
+    }
+}
--- RestaurantUI/Pages/Admin/Orders/OrderDetails.cshtml.cs
+++ RestaurantUI/Pages/Admin/Orders/OrderDetails.cshtml.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Restaurant.DAL.Interfaces;
+using Restaurant.Models;
+using Restaurant.Models.ViewModels;
 
 namespace RestaurantUI.Pages.Admin.Orders
 {
     public class OrderDetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderDetailsViewModel OrderDetailsVm { get; set; }
+
+        public OrderDetailsModel(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
+        }
+       
+        public async Task OnGet(int id)
+        {
+            OrderDetailsVm = new()
+            {
+                order = await _unitOfWork.OrderRepo.GetById(
+                    o => o.Id == id, includeProperties: "ApplicationUser"),
+                orderDetails = await _unitOfWork.OrderDetailsRepo.GetAll(o => o.Id == id)
+            };
+              
+           
+          
         }
     }
 }
